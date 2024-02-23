5e163f46df33a095490d365992b79f984f5a1fde
Carlos Brighton Saldaña Ortiz <cbrghton@gmail.com>
2024-02-22 20:24:14 -0600

    🔥 (feat!): Remove useless components
    Remove useless components

--- src/components/TheWelcome.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="container mx-auto flex flex-col gap-10 rounded-md bg-stone-500 py-10 text-center">
-    <div class="text-5xl font-bold">{{ $t('home.presentation') }}</div>
-    <div>Realizado por Carlos Brighton Saldaña Ortiz</div>
-    <div><a href="">Repositorio</a></div>
-  </div>
-</template>
