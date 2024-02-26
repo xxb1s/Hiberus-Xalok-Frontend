4ec05e44b7d743081e6bccd7d48f92a192cc0b7d
Carlos Brighton Saldaña Ortiz <cbrghton@gmail.com>
2024-02-26 13:28:33 -0600

    🔧 (feat!): Add configurations for datepicker
    Add configurations for datepicker library in main.js and tailwind configurations

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import es from './locales/es.json'
+import VueTailwindDatepicker from 'vue-tailwind-datepicker';
 
 const app = createApp(App)
 
@@ -19,4 +20,5 @@ const i18n = createI18n({
 
 app.use(router)
 app.use(i18n)
+app.use(VueTailwindDatepicker)
 app.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 import forms from '@tailwindcss/forms'
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/vue-tailwind-datepicker/**/*.js'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'vtd-primary': colors.zinc,
+        'vtd-secondary': colors.zinc
+      }
+    }
   },
   plugins: [forms]
 }
