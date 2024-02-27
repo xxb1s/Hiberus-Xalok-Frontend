33569bfcd6728cc1d4bf2de608a494c97413ba71
Carlos Brighton Saldaña Ortiz <cbrghton@gmail.com>
2024-02-26 23:00:21 -0600

    🚧 (feat!): WIP Add validations to forms
    Add the validations to drivers and vehicles forms

--- src/components/DriverForm.vue
+++ src/components/DriverForm.vue
@@ -1,5 +1,46 @@
 <script setup>
 import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/yup'
+import { object, string, setLocale } from 'yup';
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n();
+
+setLocale({
+  mixed: {
+    required: ({ path }) => {
+      return t('validations.required', { field: t('drivers.' + path)})
+    }
+  },
+  string: {
+    max: ({ path, max }) => {
+      return t('validations.max', { field: t('drivers.' + path), max: max })
+    },
+    min: ({ path, min }) => {
+      return t('validations.min', { field: t('drivers.' + path), min: min })
+    },
+    length: ({ path, length }) => {
+      return t('validations.length', { field: t('drivers.' + path), length: length })
+    }
+  }
+})
+
+const schema = toTypedSchema(
+  object({
+    name: string().required().max(255).min(3),
+    surname: string().required().max(255).min(3),
+    licence: string().required().length(1)
+  })
+)
+
+const { errors, defineField, meta } = useForm({
+  validationSchema: schema
+})
+
+const [name, nameAttrs] = defineField('name');
+const [surname, surnameAttrs] = defineField('surname');
+const [licence, licenceAttrs] = defineField('licence');
 </script>
 
 <template>
@@ -9,7 +50,7 @@ import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
     </RouterLink>
   </div>
 
-  <form action="" class="bg-stone-400 rounded-lg p-3 space-y-12">
+  <form class="bg-stone-400 rounded-lg p-3 space-y-12">
     <h2 class="text-2xl">
       {{ $t('drivers.create') }}
     </h2>
@@ -19,28 +60,31 @@ import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
       <div class="col-start-1 col-end-7 md:col-end-3">
         <label for="name" class="block text-sm font-medium leading-6">{{ $t('drivers.name') }}</label>
         <div class="mt-2">
-          <input type="text" name="name" id="name" autocomplete="given-name" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.namePlaceholder')"/>
+          <input type="text" name="name" id="name" v-model="name" v-bind="nameAttrs" autocomplete="given-name" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.namePlaceholder')"/>
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.name }}</span>
       </div>
 
       <div class="col-start-1 col-end-7 md:col-start-3 md:col-end-5">
         <label for="surname" class="block text-sm font-medium leading-6 text-gray-900">{{ $t('drivers.surname') }}</label>
         <div class="mt-2">
-          <input type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.surnamePlaceholder')" />
+          <input type="text" name="surname" id="surname" v-model="surname" v-bind="surnameAttrs" autocomplete="family-name" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.surnamePlaceholder')" />
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.surname }}</span>
       </div>
 
       <div class="md:col-start-5 md:row-span-2 col-start-1 col-end-7 items-center text-center order-last">
-        <span class="md:w-fit w-auto bg-stone-300 text-zinc-800 p-2 rounded hover:bg-stone-800 hover:text-zinc-200 active:ring active:ring-zinc-200 focus:ring focus:ring-zinc-200 flex items-center m-auto">
+        <button class="md:w-fit w-auto bg-stone-300 text-zinc-800 p-2 rounded hover:bg-stone-800 hover:text-zinc-200 active:ring active:ring-zinc-200 focus:ring focus:ring-zinc-200 flex items-center m-auto" v-if="(meta.touched && meta.valid)">
           {{ $t('drivers.create') }} <PlusIcon class="h-8 justify-center sm:h-12"/>
-        </span>
+        </button>
       </div>
 
       <div class="col-start-1 col-end-7 md:col-end-3">
         <label for="licence" class="block text-sm font-medium leading-6 text-gray-900">{{ $t('drivers.licence') }}</label>
         <div class="mt-2">
-          <input type="text" name="licence" id="licence" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.licencePlaceholder')"/>
+          <input type="text" name="licence" id="licence" v-model="licence" v-bind="licenceAttrs" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('drivers.licencePlaceholder')"/>
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.licence }}</span>
       </div>
     </div>
   </form>
--- src/components/VehicleForm.vue
+++ src/components/VehicleForm.vue
@@ -1,5 +1,48 @@
 <script setup>
 import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/yup'
+import { object, string, setLocale } from 'yup';
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n();
+
+setLocale({
+  mixed: {
+    required: ({ path }) => {
+      return t('validations.required', { field: t('vehicles.' + path)})
+    }
+  },
+  string: {
+    max: ({ path, max }) => {
+      return t('validations.max', { field: t('vehicles.' + path), max: max })
+    },
+    min: ({ path, min }) => {
+      return t('validations.min', { field: t('vehicles.' + path), min: min })
+    },
+    length: ({ path, length }) => {
+      return t('validations.length', { field: t('vehicles.' + path), length: length })
+    }
+  }
+})
+
+const schema = toTypedSchema(
+  object({
+    brand: string().required().max(255).min(1),
+    model: string().required().max(255).min(1),
+    plate: string().required().max(10).min(3),
+    licence: string().required().length(1)
+  })
+)
+
+const { errors, defineField, meta } = useForm({
+  validationSchema: schema
+});
+
+const [brand, brandAttrs] = defineField('brand');
+const [model, modelAttrs] = defineField('model');
+const [plate, plateAttrs] = defineField('plate');
+const [licence, licenceAttrs] = defineField('licence');
 </script>
 
 <template>
@@ -9,7 +52,7 @@ import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
     </RouterLink>
   </div>
 
-  <form action="" class="bg-stone-400 rounded-lg p-3 space-y-12">
+  <div class="bg-stone-400 rounded-lg p-3 space-y-12">
     <h2 class="text-2xl">
       {{ $t('vehicles.create') }}
     </h2>
@@ -19,38 +62,42 @@ import { ArrowLongLeftIcon, PlusIcon } from '@heroicons/vue/16/solid/index.js'
       <div class="col-start-1 col-end-7 md:col-end-3">
         <label for="brand" class="block text-sm font-medium leading-6">{{ $t('vehicles.brand') }}</label>
         <div class="mt-2">
-          <input type="text" name="brand" id="brand" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.brandPlaceholder')"/>
+          <input type="text" v-model="brand" v-bind="brandAttrs" name="brand" id="brand" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.brandPlaceholder')"/>
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.brand }}</span>
       </div>
 
       <div class="col-start-1 col-end-7 md:col-start-3 md:col-end-5">
         <label for="model" class="block text-sm font-medium leading-6 text-gray-900">{{ $t('vehicles.model') }}</label>
         <div class="mt-2">
-          <input type="text" name="model" id="model" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.modelPlaceholder')" />
+          <input type="text" v-model="model" v-bind="modelAttrs" name="model" id="model" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.modelPlaceholder')" />
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.model }}</span>
       </div>
 
       <div class="md:col-start-5 md:row-span-2 col-start-1 col-end-7 items-center text-center order-last">
-        <span class="md:w-fit w-auto bg-stone-300 text-zinc-800 p-2 rounded hover:bg-stone-800 hover:text-zinc-200 active:ring active:ring-zinc-200 focus:ring focus:ring-zinc-200 flex items-center m-auto">
+        <button class="md:w-fit w-auto p-2 rounded flex items-center m-auto bg-stone-300 text-zinc-800 hover:bg-stone-800 hover:text-zinc-200 active:ring active:ring-zinc-200 focus:ring focus:ring-zinc-200" v-if="(meta.touched && meta.valid)">
           {{ $t('vehicles.create') }} <PlusIcon class="h-8 justify-center sm:h-12"/>
-        </span>
+        </button>
       </div>
 
       <div class="col-start-1 col-end-7 md:col-end-3">
         <label for="plate" class="block text-sm font-medium leading-6 text-gray-900">{{ $t('vehicles.plate') }}</label>
         <div class="mt-2">
-          <input type="text" name="plate" id="plate" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.platePlaceholder')"/>
+          <input type="text" v-model="plate" v-bind="plateAttrs" name="plate" id="plate" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.platePlaceholder')"/>
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.plate }}</span>
       </div>
 
       <div class="col-start-1 col-end-7 md:col-start-3 md:col-end-5">
         <label for="licence" class="block text-sm font-medium leading-6 text-gray-900">{{ $t('vehicles.licence') }}</label>
         <div class="mt-2">
-          <input type="text" name="licence" id="licence" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.licencePlaceholder')"/>
+          <input type="text" v-model="licence" v-bind="licenceAttrs" name="licence" id="licence" class="block w-full rounded-md bg-zinc-200 border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6" :placeholder="$t('vehicles.licencePlaceholder')"/>
         </div>
+        <span class="text-red-700 text-sm font-light">{{ errors.licence }}</span>
       </div>
     </div>
-  </form>
+  </div>
 </template>
 
 <style scoped>
